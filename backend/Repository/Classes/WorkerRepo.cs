a5fe7221dfbb0cf164de43956f9e53507f9274ae
tubamark98 <val.afo00@gmail.com>
2021-12-23 14:58:08 +0100

    added crud implementation for worker repo

---
+++ backend/Repository/Classes/WorkerRepo.cs
@@ -0,0 +1,55 @@
+﻿using Data;
+using Data.DB_Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository.Classes
+{
+    public class WorkerRepo : IWorkerRepo
+    {
+        private Data.DbContext db;
+
+        public WorkerRepo(Data.DbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task Add(Worker element)
+        {
+            await this.db.AddAsync(element);
+            await this.db.SaveChangesAsync();
+        }
+
+        public async Task Delete(long element)
+        {
+            var entity = await GetOne(element);
+            this.db.Remove(entity);
+            await this.db.SaveChangesAsync();
+        }
+
+        public IQueryable<Worker> GetAll()
+        {
+            return this.db.Workers.DbSet;
+        }
+
+        public async Task<Worker> GetOne(long key)
+        {
+            var entity = await(from x in db.Workers.DbSet
+                               where x.Id == key
+                               select x).FirstOrDefaultAsync();
+            return entity;
+        }
+
+        public async Task SaveDatabase()
+        {
+            await this.db.SaveChangesAsync();
+        }
+
+        public async Task Update(long oldKey, Worker element)
+        {
+            throw new System.NotImplementedException(); //not gonna do this yet, cause I might change the properties of Worker and I didnt wanna use reflections
+        }
+    }
+}
--- backend/Repository/Interfaces/IWorkerRepo.cs
+++ backend/Repository/Interfaces/IWorkerRepo.cs
@@ -1,9 +1,4 @@
 ﻿using Data.DB_Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository.Interfaces
 {
