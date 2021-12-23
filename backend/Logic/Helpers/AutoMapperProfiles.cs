523421bc16e212becf71e3d48525c6b953d959c4
tubamark98 <val.afo00@gmail.com>
2021-12-23 15:19:26 +0100

    added mapper profile

---
+++ backend/Logic/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Data.DB_Models;
+using Logic.DTO_Models;
+
+namespace Logic.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<CreateWorkerDTO, Worker>();
+            CreateMap<Worker, ListWorkersDTO>();
+        }
+    }
+}
